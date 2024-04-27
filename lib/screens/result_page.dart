127e2e9118f6eb17af4f148f1260f8f19864ca6f
Haruki1090 <haruharu.com.109607@gmail.com>
2024-04-27 13:55:07 +0900

    DetailPageの追加

--- lib/screens/result_page.dart
+++ lib/screens/result_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_github_repository/screens/detail_page.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({super.key, required this.result, required this.query});
