a66540cdd3ee4b04d2b6de8543cf0c0d2dfc1bdb
Haruki1090 <haruharu.com.109607@gmail.com>
2024-05-12 22:52:45 +0900

    適切な文章等に修正

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -7,20 +7,45 @@ class CreateAccountScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create Account'),
+        title: const Text('ようこそ'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Create Account Screen'),
-            ElevatedButton(
-              child: Text('Go to Home Screen'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('アカウント新規作成', style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 30),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'メールアドレス',
+                ),
+              ),
+              const SizedBox(height: 40),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'パスワード',
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'パスワード(確認)',
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  // ユーザー登録処理
+                },
+                child: const Text('アカウント情報を登録'),
+              ),
+            ],
+          ),
         ),
       ),
     );
