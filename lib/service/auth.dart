31d2f6022d4abcafbc9eb995b781c552961ea34f
Haruki1090 <haruharu.com.109607@gmail.com>
2024-05-20 00:00:30 +0900

    Create関数修正

--- lib/service/auth.dart
+++ lib/service/auth.dart
@@ -2,6 +2,8 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:search_github_repository/screens/home_screen.dart';
+import 'package:search_github_repository/utils/loading_indicator.dart';
 
 // アカウント新規作成処理を行う関数
 Future<void> createUserWithEmailAndPassword({
@@ -9,12 +11,25 @@ Future<void> createUserWithEmailAndPassword({
   required String email,
   required String password,
 }) async {
+  showLoadingIndicator(context); // ローディングインジケーターを表示
   try {
     await FirebaseAuth.instance.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
+    hideLoadingIndicator(context); // ローディングインジケーターを非表示
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('アカウントを作成に成功しました。'), backgroundColor: Colors.green),
+    );
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (context) => const HomeScreen(title: 'Search GitHub Repository'),
+      ),
+          (route) => false,
+    );
   } on FirebaseAuthException catch (e) {
+    hideLoadingIndicator(context); // ローディングインジケーターを非表示
     String errorMessage;
     if (e.code == 'weak-password') {
       errorMessage = '提供されたパスワードは弱すぎます。';
@@ -23,12 +38,15 @@ Future<void> createUserWithEmailAndPassword({
     } else {
       errorMessage = '認証中にエラーが発生しました: ${e.message}';
     }
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(errorMessage)),
+      SnackBar(content: Text(errorMessage), backgroundColor: Colors.red),
     );
   } catch (e) {
+    hideLoadingIndicator(context); // ローディングインジケーターを非表示
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('エラーが発生しました: $e')),
+      SnackBar(content: Text('エラーが発生しました: $e'), backgroundColor: Colors.red),
     );
   }
 }
