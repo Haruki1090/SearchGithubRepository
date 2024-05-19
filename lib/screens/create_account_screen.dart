5e05028a9586b705ac1fc4106472067306064285
Haruki1090 <haruharu.com.109607@gmail.com>
2024-05-19 22:03:04 +0900

    フォーム入力に関する基礎

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-class CreateAccountScreen extends StatelessWidget {
+class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
+
+  @override
+  State<CreateAccountScreen> createState() => _CreateAccountScreenState();
+}
+
+class _CreateAccountScreenState extends State<CreateAccountScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,22 +27,25 @@ class CreateAccountScreen extends StatelessWidget {
             children: <Widget>[
               const Text('アカウント新規作成', style: TextStyle(fontSize: 20)),
               const SizedBox(height: 30),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'メールアドレス',
                 ),
               ),
               const SizedBox(height: 40),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'パスワード',
                 ),
               ),
               const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                controller: confirmPasswordController,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'パスワード(確認)',
                 ),
@@ -40,9 +53,70 @@ class CreateAccountScreen extends StatelessWidget {
               const SizedBox(height: 40),
               ElevatedButton(
                 onPressed: () {
-                  // ユーザー登録処理
+                  if (passwordController.text !=
+                      confirmPasswordController.text) {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('エラー'),
+                          content: const Text('パスワードが一致しません'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('閉じる'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else if (emailController.text.isEmpty ||
+                      passwordController.text.isEmpty ||
+                      confirmPasswordController.text.isEmpty) {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('エラー'),
+                          content: const Text('全ての項目を入力してください'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('閉じる'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('入力内容を確認'),
+                            content: Column(
+                              children: [
+                                Text('メールアドレス: ${emailController.text}'),
+                                Text('パスワード: ${passwordController.text}'),
+                                ],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('閉じる'),
+                              ),
+                            ],
+                          );
+                        });
+                  }
                 },
-                child: const Text('アカウント情報を登録'),
+                child: const Text('入力内容を確認'),
               ),
             ],
           ),
