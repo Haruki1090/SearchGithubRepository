836cce845d7eee59f5054b91a923fa2defcf29ed
Haruki1090 <haruharu.com.109607@gmail.com>
2024-05-20 23:05:15 +0900

    ユーザーのログイン状況を監視

--- lib/provider/search_history_provider.dart
+++ lib/provider/search_history_provider.dart
@@ -1,7 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:search_github_repository/models/search_history_model.dart';
-import 'package:search_github_repository/service/firestore_service.dart';
+import 'package:search_github_repository/service/auth.dart';
 
 final searchHistoryProvider = StreamProvider<List<SearchHistory>>((ref) {
-  return getSearchHistory();
+  final user = ref.watch(firebaseAuthProvider).currentUser;
+  if (user == null) {
+    return Stream.value([]);  // ユーザーがログインしていない場合は空のリストを返す
+  }
+  return FirebaseFirestore.instance
+      .collection('users')
+      .doc(user.uid)
+      .collection('search_history')
+      .orderBy('createdAt', descending: true)
+      .snapshots()
+      .map((snapshot) =>
+      snapshot.docs.map((doc) => SearchHistory.fromJson(doc.data())).toList());
 });
