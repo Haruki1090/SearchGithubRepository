b84d0c8129765c31a5bb0c4fa3e6324fb1ea9c8e
Haruki1090 <haruharu.com.109607@gmail.com>
2024-05-20 21:57:54 +0900

    検索ワードを各アカウントのデータに保存する

--- lib/service/firestore_service.dart
+++ lib/service/firestore_service.dart
@@ -1,15 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:search_github_repository/models/search_history_model.dart';
 
-Future createSearchHistory(String query) async {
+Future<void> createSearchHistory(String query) async {
   final user = FirebaseAuth.instance.currentUser;
-  final queryHistory = query;
-  final createdAt = Timestamp.now();
-  final document = FirebaseFirestore.instance.collection('users').doc(user!.uid).collection('search_history').doc();
-  await document.set({
-    'queryHistory': queryHistory,
-    'createdAt': createdAt,
-    'updatedAt': createdAt,
-    'favorite': false,
-  });
+  if (user == null) {
+    throw Exception('ユーザーがログインしていません');
+  }
+
+  final searchHistory = SearchHistory(
+    query: query,
+    createdAt: Timestamp.now(),
+    updatedAt: Timestamp.now(),
+  );
+
+  final document = FirebaseFirestore.instance
+      .collection('users')
+      .doc(user.uid)
+      .collection('search_history')
+      .doc();
+
+  await document.set(searchHistory.toJson());
 }
